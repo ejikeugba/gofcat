db2a37a8b157a8e4f4042b153456e6282b5f3c6d
ejike ugba <ejike.ugba@outlook.com>
2022-02-13 01:27:38 +0100

    Update compfn.R

--- R/compfn.R
+++ R/compfn.R
@@ -6,28 +6,38 @@ compfn <- function(model, modeltype){
     est <- cf[-c(1:r-1)]
   } else est <- model$coefficients
   if (modeltype == "vglm") {
-    mc <- methods::slot(model, "call")
-    m <- methods::slot(model, "model")
+    mc <- model@call
+    m <- model@model
     if (is.matrix(m)) m <- as.data.frame(m)
-    if (dim(m)[1L] == 0)
-      stop("original dataframe not available! consider adding model=TRUE
-           in vglm function and try again", call. = FALSE)
-    Terms <- methods::slot(model, "terms")
+    if (dim(m)[1L] == 0L){
+      model <- update(model, model = TRUE)
+      m <- model@model
+    }
+    mkeep <- m
+    if(!is.null(dim(model.response(m)))) m <- longDt(model, m)
   } else {
     mc <- model$call
     m <- model$model
+    mkeep <- m
+    if(!is.null(dim(model.response(m)))) m <- longDt(model, m)
     if (modeltype == "serp"){
       model$terms <- model$Terms
       model$xlevels <- stats::.getXlevels(model$terms, m)
     }
-    Terms <- model$terms
   }
-  grpmod <- c("clm", "polr", "serp")
-  if (any(modeltype == grpmod)) n <- model$nobs else n <- model@misc$n
-  y <- as.numeric(stats::model.response(m))
+  dmr <- is.null(dim(model.response(mkeep)))
+  if(!dmr)
+    model <-   stats::update(model, formula=paste0(
+      "resp"," ~ ", as.character(stats::formula(model)[3L])), data=m)
+  if (modeltype=="vglm" &&
+      length(unique(VGAM::model.frame(model)[,1L]))==2L)
+    stop("test not available for binary models!", call. = FALSE)
+  Terms <- stats::terms(model)
+  y <- if(dmr) as.numeric(stats::model.response(m)) else as.numeric(m[,1L])
   x <- stats::model.matrix(Terms, m)
   xint <- match("(Intercept)", colnames(x), nomatch = 0L)
   x <- x[, -xint, drop = FALSE]
+  n <- length(y)
   mi <- m
   m <- ncleaner(m)
   list(model=model, est=est, x=x, y=y, mc=mc, mi=mi, m=m, n=n)
